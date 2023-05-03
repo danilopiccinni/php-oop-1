5900f734e6918d126595695717d3bb0b964d546b
danilopiccinni <danilopiccinni@gmail.com>
2023-05-03 14:46:13 +0200

    bonus2(abbozzato)

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$movies[] = new Movie ('avenger', 'avenger', 'pinko', ['azione' , 'divertente'] ,'2000', 'blablalvlballbalala');
+$movies[] = new Movie ('spiderman', 'spider', 'pallo', ['comico'] ,'2222', 'blalvlalalfldlalsdlasdldas');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
+$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php 
 
 require_once './Models/Movie.php';
-
-$movies[] = new Movie ('avenger', 'avenger', 'pinko', ['azione' , 'divertente'] ,'2000', 'blablalvlballbalala');
-$movies[] = new Movie ('spiderman', 'spider', 'pallo', ['comico'] ,'2222', 'blalvlalalfldlalsdlasdldas');
-$movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950', 'blalvlalalfldlalsdlasdldasasdasdsadasdasds');
-
+require_once 'db.php';
 
 ?>
 
@@ -16,18 +12,29 @@ $movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
 </head>
 <body>
+
+    <?php include './Partials/nav.php' ?>
+
+    <h1 class="titolo">Lista film</h1>
     
-    <table>
+    <table class="table table-dark table-striped">
         <thead>
-            <th>Titolo</th>
-            <th>Titolo Originale</th>
-            <th>Regista</th>
-            <th>Genere</th>
-            <th>Anno di Produzione</th>
-            <th>Descrizione</th>
-            <th>Vecchio</th>
+                <th>Titolo</th>
+                <th>Titolo Originale</th>
+                <th>Regista</th>
+                <th>Genere</th>
+                <th>Anno di Produzione</th>
+                <th>Descrizione</th>
+                <th>Vecchio</th>
         </thead>
 
         <tbody>
@@ -49,6 +56,11 @@ $movies[] = new Movie ('hulk', 'hulk', 'kaio', ['drammatico' , 'azione'] ,'1950'
             ?>
         </tbody>
     </table>
+
+    <?php include './Partials/footer.php' ?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
 </body>
 </html>
